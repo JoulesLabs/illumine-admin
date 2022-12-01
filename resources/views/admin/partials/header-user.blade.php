0bd05b30c939d5bd2dde6eaa073395e001f27e7c
rsayed007 <romansyed007@gmail.com>
2022-12-01 15:47:24 +0600

    remove unnecessary file and code from sidebar, add breadcrumbs

--- resources/views/admin/partials/header-user.blade.php
+++ resources/views/admin/partials/header-user.blade.php
@@ -2,16 +2,7 @@
         <!-- <img alt="image" src="../assets/img/avatar/avatar-1.png" class="rounded-circle mr-1"> -->
         <div class="d-sm-none d-lg-inline-block">{{auth_admin()->name}}</div></a>
     <div class="dropdown-menu dropdown-menu-right">
-        <!-- <div class="dropdown-title">Logged in 5 min ago</div> -->
-        <!-- <a href="features-profile.html" class="dropdown-item has-icon">
-            <i class="far fa-user"></i> Profile
-        </a>
-        <a href="features-activities.html" class="dropdown-item has-icon">
-            <i class="fas fa-bolt"></i> Activities
-        </a>
-        <a href="features-settings.html" class="dropdown-item has-icon">
-            <i class="fas fa-cog"></i> Settings
-        </a> -->
+
         <a href="{{route('admin::user.change_password_page')}}" class="dropdown-item has-icon">
             <i class="fas fa-key"></i></i> Change Password
         </a>
